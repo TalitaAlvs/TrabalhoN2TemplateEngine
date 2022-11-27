119b40c3c899f857bda1ec25ffb57100bc9cc08c
Talita Alves <67616021+TalitaAlvs@users.noreply.github.com>
2022-11-27 12:21:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars')
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/HTML/index.html");
-});
+// Config
+    // Template Engine
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
+    // Conexão com o MySQL
+
+// Rotas
+
+        app.get('/cadastro', function(req, res){
+            res.render('home')
+        })
 
 app.listen(3000, function(){
-    console.log("Servidor Rodando");
+    console.log("Servidor Rodando na url http://localhost:3000");
 });
