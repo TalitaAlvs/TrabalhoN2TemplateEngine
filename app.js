9f90e467b3465780f4d330cbf92f69a24e33274d
Leilanny Nascimento <usilva055@gmail.com>
2022-11-27 14:43:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const app = express();
-const handlebars = require('express-handlebars')
-
+const express = require('express')
+const handlebars= require('express-handlebars')
+//const handlebars = require('express-handlebars')
 const port = 3000
+
+const app = express();
 // Config
 // Template Engine
-app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-app.set('view engine', 'handlebars')
 
+//app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+app.set('views', './views')
 app.use('/public', express.static(__dirname + '/public'))
 // Conexão com o MySQL
 
